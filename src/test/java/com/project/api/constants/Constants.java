b9aba10cbd4cca1b7ec4d7ac7a2d3b8d5a16551f
ranadeep-banik137 <42244847+ranadeep-banik137@users.noreply.github.com>
2018-08-17 18:13:24 +0530

    Add files via upload

---
+++ src/test/java/com/project/api/constants/Constants.java
@@ -0,0 +1,12 @@
+package com.project.api.constants;
+
+public interface Constants {
+
+	String BASE_URI = "baseUri";
+	
+	String BASE_PATH = "basePath";
+	
+	String SAML_HEADER = "X-Saml-Token";
+	
+	String IP_ID_HEADER = "X-Ip-Id";
+}
---
+++ src/test/java/com/project/api/constants/FlatFile.java
@@ -0,0 +1,16 @@
+package com.project.api.constants;
+
+public interface FlatFile {
+
+	String POST_CUSTOMER_REQUEST_PAYLOAD = "customer-request-payload.properties";
+	
+	String POST_CUSTOMER_RESPONSE_PAYLOAD = "customer-response-payload.properties";
+	
+	String POST_CUSTOMER_RESPONSE_SCHEMA = "response-JSON-schema.json";
+	
+	String POST_CUSTOMER_REQUEST_SCHEMA = "request-JSON-schema.json";
+	
+	String GET_CUSTOMER_RESPONSE_PAYLOAD = "get-customer-details-response-payload.properties";
+	
+	String GET_CUSTOMER_RESPONSE_SCHEMA = "response-JSON-schema.json";
+}

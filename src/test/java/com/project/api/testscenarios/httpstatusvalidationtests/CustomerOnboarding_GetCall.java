f1a00a419a63bb2b2e263e627b034a5004ace6f0
ranadeep-banik137 <42244847+ranadeep-banik137@users.noreply.github.com>
2018-08-18 05:32:38 +0530

    Update CustomerOnboarding_GetCall.java

--- src/test/java/com/project/api/testscenarios/httpstatusvalidationtests/CustomerOnboarding_GetCall.java
+++ src/test/java/com/project/api/testscenarios/httpstatusvalidationtests/CustomerOnboarding_GetCall.java
@@ -2,11 +2,16 @@ package com.project.api.testscenarios.httpstatusvalidationtests;
 
 import io.restassured.http.ContentType;
 
+import java.lang.reflect.Method;
 import java.security.InvalidAlgorithmParameterException;
+import java.util.logging.Logger;
 
 import org.apache.http.HttpStatus;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.internal.TestResult;
 
 import com.project.api.constants.ApiCallConfig;
 import com.project.api.constants.Constants;
@@ -22,7 +27,7 @@ import com.project.app.api_test_v1.utilities.RestCalls;
 @Test(alwaysRun = true, groups=ApiCallConfig.GET_CUSTOMER_API)
 public class CustomerOnboarding_GetCall {
 
-	
+	private static Logger LOGGER = Logger.getLogger(CustomerOnboarding_GetCall.class.getName());
 	RestCalls restCall;
 	String baseUri;
 	String basePath;
@@ -41,7 +46,7 @@ public class CustomerOnboarding_GetCall {
 	 * basic configuration set up done for Initializing the call
 	 */
 	@BeforeMethod
-	private void setUp() {
+	private void setUp(Method method) {
 		this.baseUri = DataUtil.fetchDataFromTestDataFile(Constants.BASE_URI);
 		this.basePath = DataUtil.fetchDataFromTestDataFile(Constants.BASE_PATH);
 		
@@ -52,6 +57,7 @@ public class CustomerOnboarding_GetCall {
 		} catch (InvalidAlgorithmParameterException exceptionObject) {
 			exceptionObject.printStackTrace();
 		}
+		LOGGER.info("Method initiating ".concat(method.getName()));
 	}
 	
 	/**
@@ -210,4 +216,9 @@ public class CustomerOnboarding_GetCall {
 		this.restCall.assertResponseCode(HttpStatus.SC_NOT_ACCEPTABLE);
 	}
 	
+	@AfterMethod
+	private void conclude(ITestResult result) {
+		String testName = result.getMethod().getMethodName();
+		LOGGER.info(result.getStatus() == TestResult.SUCCESS ? "Execution successful. Method ".concat(testName).concat(" PASSED") : (result.getStatus() == TestResult.SKIP ? "Method ".concat(testName).concat(" SKIPPED") : "Method ".concat(testName).concat(" FAILED")));
+	}
 }
